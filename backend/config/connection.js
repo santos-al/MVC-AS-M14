34ec03e651e2e5e38e576232152946c556197f37
Xandi <santosal2018@gmail.com>
2023-08-06 19:52:36 -0400

    fix .env issue

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,11 +1,15 @@
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
+require('dotenv').config('./env');
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
