6551fdfc3ca78d98c49efc62baa91d51bf1d65b6
Xandi <santosal2018@gmail.com>
2023-08-06 20:06:03 -0400

    fix remaining errors

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 const Sequelize = require('sequelize');
+const path = require('path');
 
-require('dotenv').config('./env');
+require('dotenv').config(path.join(__dirname, '.env'));
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
+    "tech_blog_db", 
+    'root', 
+    'root1234', 
     {
       host: 'localhost',
       dialect: 'mysql',
